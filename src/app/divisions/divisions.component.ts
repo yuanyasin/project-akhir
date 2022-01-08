9e1fd5db47659191f4caced9b314b630ec335f56
yuanyasin <yuangaffar@gmail.com>
2022-01-08 15:03:20 +0700

    baru

--- src/app/divisions/divisions.component.ts
+++ src/app/divisions/divisions.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Division } from '../model/division.model';
+import { DivisionsService } from '../services/division.service';
 
 @Component({
-  selector: 'app-divisions',
+  selector: 'app-division',
   templateUrl: './divisions.component.html',
   styleUrls: ['./divisions.component.css']
 })
 export class DivisionsComponent implements OnInit {
 
-  constructor() { }
+  divisions?:Division[];
+  currentDivision:Division={};
+  name='';
+  constructor(private divisionsService:DivisionsService) { }
 
   ngOnInit(): void {
+    this.retrieveDivision()
   }
 
+  retrieveDivision():void{
+    this.divisionsService.getAll()
+      .subscribe(
+        data=>{
+          this.divisions=data;
+          console.log(data);
+        },
+        error=>{
+          console.log(error)
+        }
+      )
+
+  }
 }
